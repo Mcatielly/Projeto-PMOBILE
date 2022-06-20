cbb45aca6afd03f026d8b3defbcadba73960a572
Mcatielly <103471775+Mcatielly@users.noreply.github.com>
2022-06-20 00:08:50 -0300

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/HomePage';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: HomePage(),
+    ),
+  );
+}
